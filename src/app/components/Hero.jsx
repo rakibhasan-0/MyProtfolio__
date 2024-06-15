f3e30785ff830d377d192535cf3707120d5935b3
rakib <rakibhasan_1@hotmail.com>
2024-06-15 14:08:37 +0200

    aminitions have been added

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,27 +1,51 @@
-import React from 'react'
-import { MY_INFO } from '../../../utils/constant'
-import profilePicture from '../../../public/myImage__.jpg'
-import Image from 'next/image'
+"use client"
+import { MY_INFO } from '../../../utils/constant';
+import profilePicture from '../../../public/myImage__.jpg';
+import Image from 'next/image';
+import { motion } from "framer-motion";
+
 
 export const Hero = () => {
   return (
-    <div className = "pb-4 lg:mb-35">
-        <div className="flex flex-wrap items-center justify-between">
-            <div className ="w-full lg:w-1/2 ">
-                <div className = "my-20 flex flex-col items-center lg:items-start">
-                    <span className = "bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-transparent text-2xl font-thin lg:text-5xl tracking-tight">
-                        Gazi Md Rakibul Hasan
-                    </span>
-                    <p className=" text-white my-20 sm:my-10 font-light">{MY_INFO}</p>
-                </div>
-            </div>
-
-            <div className="w-full lg:w-1/2">
-                <div className="flex justify-center ml-20">
-                    <Image src={profilePicture} alt="myImage" width={400} height={450} className='rounded-md'/>
-                </div>
-            </div>
+    <div className="pb-4 lg:mb-35">
+      <div className="flex flex-wrap items-center justify-between">
+        <div className="w-full lg:w-1/2">
+          <div className="my-20 flex flex-col items-center lg:items-start">
+            <motion.h2
+                initial={{ x: -100, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.7}}
+                className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-transparent text-2xl font-thin lg:text-5xl tracking-tight"
+            >
+              Gazi Md Rakibul Hasan
+            </motion.h2>
+            <motion.p
+                initial={{ x: -100, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.7, delay: 0.5}}
+                className="text-white my-20 sm:my-10 font-light"
+            >
+              {MY_INFO}
+            </motion.p>
+          </div>
         </div>
+        <div className="w-full lg:w-1/2">
+        <motion.div 
+            className="flex justify-center ml-20"
+            initial={{ x: 100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.7, delay: 1.2}}
+        >
+            <Image
+              src={profilePicture}
+              alt="myImage"
+              width={400}
+              height={450}
+              className="rounded-md"
+            />
+          </motion.div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
--- src/app/components/Technology.jsx
+++ src/app/components/Technology.jsx
@@ -1,9 +1,11 @@
+"use client";
 import React from 'react'
 import { FaReact, FaJava, FaJsSquare, FaPostgres } from "react-icons/fa";
 import {TbBrandNextjs} from "react-icons/tb"
 import CLang from '../../../public/C.png'
 import Postgres from '../../../public/postgres.png'
 import Image from 'next/image'
+import { motion } from "framer-motion";
 
 
 
@@ -12,26 +14,70 @@ export const Technology = () => {
     <div className="border-b border-neutral-800 pb-24">
       <h1 className="my-20 text-center text-4xl">Technology</h1>
 
-      <div className="flex flex-wrap justify-center gap-5">
-        <div className="border-4 rounded-2xl border-neutral-800 p-4">
+      <motion.div 
+        className="flex flex-wrap justify-center gap-5"
+      >
+        <motion.div 
+          className="border-4 rounded-2xl border-neutral-800 p-4"
+          initial={{y: 10}}
+          animate={{y: 0}}
+          transition={{duration: 1, ease: "linear", repeat: Infinity, repeatType: "reverse"}}
+        >
           <FaReact className="text-4xl text-blue-500" />
-        </div>
-        <div className="border-4 rounded-2xl border-neutral-800 p-4">
+        </motion.div>
+
+        <motion.div 
+          className="border-4 rounded-2xl border-neutral-800 p-4"
+          initial={{y: 15}}
+          animate={{y: 2}}
+          transition={{duration: 1.5, ease: "linear", repeat: Infinity, repeatType: "reverse"}}
+        
+        >
           <FaJava className="text-4xl text-red-600" />
-        </div>
-        <div className="border-4 rounded-2xl border-neutral-800 p-4">
+        </motion.div>
+
+
+        <motion.div 
+          className="border-4 rounded-2xl border-neutral-800 p-4"
+          initial={{y: 20}}
+          animate={{y: 4}}
+          transition={{duration: 2, ease: "linear", repeat: Infinity, repeatType: "reverse"}}
+        >
           <FaJsSquare className="text-4xl text-yellow-500" />
-        </div>
-        <div className="border-4 rounded-2xl border-neutral-800 p-4">
+        </motion.div>
+
+
+        <motion.div 
+          className="border-4 rounded-2xl border-neutral-800 p-4"
+          initial={{y: 25}}
+          animate={{y: 6}}
+          transition={{duration: 2.5, ease: "linear", repeat: Infinity, repeatType: "reverse"}}
+        >
           <Image src={CLang} alt="C" width={35} height={35} />
-        </div>
-        <div className="border-4 rounded-2xl border-neutral-800 p-4">
+        </motion.div>
+
+        <motion.div 
+          className="border-4 rounded-2xl border-neutral-800 p-4"
+          initial={{y: 30}}
+          animate={{y: 8}}
+          transition={{duration: 2.8, ease: "linear", repeat: Infinity, repeatType: "reverse"}}        
+        >
             <Image src={Postgres} alt="Postgres" width={40} height={40} />
-        </div>
-        <div className="border-4 rounded-2xl border-neutral-800 p-4">
+        </motion.div>
+
+
+        <motion.div 
+          className="border-4 rounded-2xl border-neutral-800 p-4"
+          initial={{y: 35}}
+          animate={{y: 10}}
+          transition={{duration: 3, ease: "linear", repeat: Infinity, repeatType: "reverse"}}
+        >
             <TbBrandNextjs className="text-4xl text-white"/>
-        </div>
-      </div>
+        </motion.div>
+
+      </motion.div>
+
+
     </div>
   );
 }
